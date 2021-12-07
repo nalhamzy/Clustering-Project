8801d07b54e3d737090afef040e973cfcafb5bbd
nalhamzy <63153023+nalhamzy@users.noreply.github.com>
2021-12-08 00:44:56 +0100

    we

---
+++ main2.py
@@ -0,0 +1,25 @@
+from pandas.core.frame import DataFrame
+from serpapi import GoogleSearch
+import os
+import csv
+import time
+import pandas as pd
+
+import streamlit as st
+import numpy as np
+from bs4 import BeautifulSoup
+from requests import get
+import re 
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from pprint import pprint
+from requests import session
+from Questgen import main as qgen
+
+
+
+def main():
+    st.write('page loaded ')
+if __name__ == '__main__':
+	main()
