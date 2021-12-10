ac49623b86a6e75ba21e6b14bcdea02e3a4c3496
nalhamzy <63153023+nalhamzy@users.noreply.github.com>
2021-12-10 19:11:58 +0100

    updateudate

--- main.py
+++ main.py
@@ -43,28 +43,25 @@ def search_keywords(input_text):
     gsearch = GoogleSearch({
         "q": input_text, 
         "location": "Austin,Texas",
-        "num" : "5",
+        "num" : "15",
         "api_key": app_secret
     })
     result = gsearch.get_dict()
     #print(result)
     final_results= []
-    for key in result:
-        print(key)
-        #print(result[key])
 
-    #print(result['organic_results'])
     df = pd.DataFrame(columns=['link','title','text'])
     
     n_samples = 5
-  
+    print('len')
     print(len(result['organic_results']))
     for item in result['organic_results']:
+        print(item['link'])
         page_url = item['link']
         title=item['title']
         response = get(page_url)
         html_soup = BeautifulSoup(response.text, 'html.parser')
-        p_list = html_soup.find_all('p')[:10]
+        p_list = html_soup.find_all('p')[:no_paragraphs]
         st.write("\n")
         st.write("Website title: %s :"%title) 
         text = ''
